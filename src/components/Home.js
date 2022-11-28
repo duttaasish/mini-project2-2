28c885e5394e04c0a49bee23cb068d96be06819a
jobatkinIOD <jo.batkin@institutedata.com>
2022-11-28 15:26:30 +1000

    reviewed, added suggestions

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ import Translate from "./Translate";
 
 function Home() {
   return (
-    <BrowserRouter>
+    <BrowserRouter> {/* would usually put this at a higher level in index.js or App.js, but works here too */}
       <div className="mainDiv">
         <div className="homeMainDiv">
           <Link to="/countryapi">
@@ -24,7 +24,7 @@ function Home() {
       </div>
 
       <Routes>
-        <Route exact path="/countryapi" element={<Countryapi />} />
+        <Route exact path="/countryapi" element={<Countryapi />} /> {/* would probably choose better names such as CountryList and TextTranslation, but this is fine */}
         <Route exact path="/translate" element={<Translate />} />
       </Routes>
     </BrowserRouter>
